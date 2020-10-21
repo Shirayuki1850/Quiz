2e914e04c6de35c3f76731ebc669bcbb4a0b0dd1
Toyotomi1588 <66935367+Toyotomi1588@users.noreply.github.com>
2020-10-21 12:01:42 +0900

    selectのジャンル追加

--- Project1/Project1/ObjSelect.cpp
+++ Project1/Project1/ObjSelect.cpp
@@ -5,6 +5,7 @@
 #include"GameL/DrawFont.h"
 
 #include"ObjSelect.h"
+#include "SceneMain.h"
 
 //使用するネームスペース
 using namespace GameL;
@@ -12,13 +13,21 @@ using namespace GameL;
 //イニシャライズ
 void CObjSelect::Init()
 {
+	m_mou_x = 0.0f;
+	m_mou_y = 0.0f;
 
+	m_mou_l = false;
 }
 
 //アクション
 void CObjSelect::Action()
 {
+	//マウスの位置を取得
+	m_mou_x = (float)Input::GetPosX();
+	m_mou_y = (float)Input::GetPosY();
 
+	//マウスのボタンの状態
+	m_mou_l = Input::GetMouButtonL();
 }
 
 //ドロー
@@ -26,8 +35,16 @@ void CObjSelect::Draw()
 {
 	float c[4] = { 1,1,1,1 };
 
-	//説明
-   //Font::StrDraw(L"ジャンルを選択してください\n", 200, 250, 40, c);
+   //説明
+   Font::StrDraw(L"ジャンルを選択してください\n", 200, 250, 40, c);
+   
+   //ジャンルをクリックする場所
+   Font::StrDraw(L"国語\n", 230, 400, 30, c);
 
+   Font::StrDraw(L"外国語\n", 230, 400, 30, c);
+
+   Font::StrDraw(L"社会\n", 230, 400, 30, c);
+
+   Font::StrDraw(L"ゲーム関連\n", 230, 400, 30, c);
 	
 }
--- Project1/Project1/ObjSelect.h
+++ Project1/Project1/ObjSelect.h
@@ -14,4 +14,8 @@ public:
 	void Action(); //アクション
     void Draw();   //ドロー
 private:
+
+	float m_mou_x; //マウスの位置X
+	float m_mou_y; //マウスの位置Y
+	bool  m_mou_l; //マウスの左ボタン
 };
