8fee4f565508e78a666b31dd8e6e0e89354c50c7
慎平 宮部 <h1201919035@hiratagakuen.onmicrosoft.com>
2020-10-21 09:37:05 +0900

    タイトル作成途中

--- Project1/Project1/SceneTItle.h
+++ Project1/Project1/SceneTItle.h
@@ -1,2 +1,17 @@
 #pragma once
 //使用するヘッダー
+#include "GameL/SceneManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//シーン：ゲームタイトル
+class CSceneTitle :public CScene
+{
+public:
+	CSceneTitle();
+	~CSceneTitle();
+	void InitScene();    //ゲームタイトルの初期化メソッド
+	void Scene();        //ゲームタイトルの初期化メソッド
+private:
+};
