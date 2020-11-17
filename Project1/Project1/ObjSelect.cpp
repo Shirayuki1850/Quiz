f51e4450b99cee8a351910160fbfe18387b2b40b
和益 松本 <h1201919010@hiratagakuen.onmicrosoft.com>
2020-11-17 11:18:41 +0900

    png表記の変更、パネル表示の処理追加、 文字の大きさの微調整等

--- Project1/Project1/ObjSelect.cpp
+++ Project1/Project1/ObjSelect.cpp
@@ -22,7 +22,12 @@ void CObjSelect::Init()
 	m_mou_y = 0.0f;
 	px;
 	py;
+	pi;
+	pl;
 	pp[4];
+	po[4];
+
+	j=0;
 
 	m_mou_l = false;
 
@@ -32,6 +37,8 @@ void CObjSelect::Init()
 //アクション
 void CObjSelect::Action()
 {
+	
+
 	//マウスの位置を取得
 	m_mou_x = (float)Input::GetPosX();
 	m_mou_y = (float)Input::GetPosY();
@@ -39,15 +46,80 @@ void CObjSelect::Action()
 	//マウスのボタンの状態
 	m_mou_l = Input::GetMouButtonL();
 
-	//pi = ((int)m_mou_x - 430) / 128;
 
 	//マウスの位置とクリックする場所で当たり判定
-	if (m_mou_x>430&&m_mou_x<490&&m_mou_y>390&&m_mou_y<450&&m_mou_l==true)
+	if (m_mou_x >=430 && m_mou_x <= 710 && m_mou_y >= 190 && m_mou_y <= 470 && m_mou_l == true)
 	{
-		Scene::SetScene(new CSceneMain());
+		pi = ((int)m_mou_x - 430) / 70;
+
+		pl = ((int)m_mou_y - 190) / 70;
+
+
+		/*switch (pl)
+		{
+		case 0:
+			switch (pi)
+			{
+			case 0: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 1: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 2: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 3: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			}
+			break;
+
+		case 1:
+			switch (pl)
+			{
+			case 0: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 1: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 2: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 3: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			}
+			break;
+		case 2:
+			switch (pl)
+			{
+			case 0: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 1: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 2: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 3: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			}
+			break;
+		case 3:
+			switch (pl)
+			{
+			case 0: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 1: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 2: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			case 3: fopen_s(&fp, "Quiz1.txt", "rb");
+				break;
+			}
+			break;
+
+		}*/
+
+				Scene::SetScene(new CSceneMain());
+
+		
 	}
 
 	//１プレイヤーが正解した時、パネルを赤にする
+
 
 	//２プレイヤーが正解した時、パネルを青にする
 
@@ -62,17 +134,20 @@ void CObjSelect::Draw()
 	RECT_F src;
 	RECT_F dst;
 
+	int p=2;
+	float x = 0;
+
    //説明
-   Font::StrDraw(L"ジャンルを選択してください", 185, 50, 30, c);
+   Font::StrDraw(L"ジャンルを選択してください", 125, 60, 45, c);
    
    //ジャンルをクリックする場所
-   Font::StrDraw(L"国語   赤色", 100, 200, 30, c);
+   Font::StrDraw(L"国語", 100, 200, 30, c);
 
-   Font::StrDraw(L"外国語  青色", 100, 300, 30, c);
+   Font::StrDraw(L"外国語", 100, 300, 30, c);
 
-   Font::StrDraw(L"社会    緑色", 100, 400, 30, c);
+   Font::StrDraw(L"社会", 100, 400, 30, c);
 
-   Font::StrDraw(L"ゲーム関連   黄色", 100, 500, 30, c);
+   Font::StrDraw(L"ゲーム関連", 100, 500, 30, c);
 
    //切り取り位置の設定
    src.m_top = 0.0f;
@@ -80,13 +155,57 @@ void CObjSelect::Draw()
    src.m_right = 128.0f;
    src.m_bottom = 128.0f;
 
-   //表示位置の設定
-   dst.m_top = 390.0f;
-   dst.m_left = 430.0f;
-   dst.m_right = 490.0f;
-   dst.m_bottom = 450.0f;
+   for (int i = 0; i < 4; i++)
+   {
+		   dst.m_top = 190.0f;
+		   dst.m_left = 300.0f + x;
+		   dst.m_right = 370.0f + x;
+		   dst.m_bottom = 260.0f;
+		   x += 100;
 
-   //描画
-   Draw::Draw(2, &src, &dst, c, 0);
-	
+	  //描画
+	  Draw::Draw(p++, &src, &dst, c, 0);
+   }
+
+   x = 0;
+
+   for (int i = 0; i < 4; i++)
+   {
+	   dst.m_top = 290.0f;
+	   dst.m_left = 300.0f + x;
+	   dst.m_right = 370.0f + x;
+	   dst.m_bottom = 360.0f;
+	   x += 100;
+
+	   //描画
+	   Draw::Draw(p++, &src, &dst, c, 0);
+   }
+
+   x = 0;
+
+   for (int i = 0; i < 4; i++)
+   {
+	   dst.m_top = 390.0f;
+	   dst.m_left = 300.0f + x;
+	   dst.m_right = 370.0f + x;
+	   dst.m_bottom = 460.0f;
+	   x += 100;
+
+	   //描画
+	   Draw::Draw(p++, &src, &dst, c, 0);
+   }
+
+   x = 0;
+
+   for (int i = 0; i < 4; i++)
+   {
+	   dst.m_top = 490.0f;
+	   dst.m_left = 300.0f + x;
+	   dst.m_right = 370.0f + x;
+	   dst.m_bottom = 560.0f;
+	   x += 100;
+
+	   //描画
+	   Draw::Draw(p++, &src, &dst, c, 0);
+   }
 }
