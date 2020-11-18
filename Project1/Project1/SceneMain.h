62f8f90caa39aae2cdadd1b8b033b5e233237fc8
知城 寒川 <h1201919023@hiratagakuen.onmicrosoft.com>
2020-11-18 14:39:17 +0900

    SceneMain.cpp,h一部変更/追加

--- Project1/Project1/SceneMain.h
+++ Project1/Project1/SceneMain.h
@@ -19,6 +19,9 @@ private:
 	wchar_t str2[100];
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
 
+	int pi;        //パネル一つ分の値X
+	int pl;        //パネル一つ分の値Y
+
 	float m_mou_x; //マウスの位置X
 	float m_mou_y; //マウスの位置Y
 	bool  m_mou_l; //マウスの左ボタン
